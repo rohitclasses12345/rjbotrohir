cc3d14518933ab8c519cfa1ce0e5e91b36cca567
rohitclasses12345 <rohitclasses12345@gmail.com>
2025-01-25 11:23:40 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,11 @@
+# Sanjay Kagra 
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+
+API_ID    = os.environ.get("API_ID", "20607064")
+API_HASH  = os.environ.get("API_HASH", "c0a09fd762681a66366cf84976f31a17")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7939391364:AAFqi5QuudQtJGoBTYbnTHgqUu_Kd6UF5EY") 
+
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
